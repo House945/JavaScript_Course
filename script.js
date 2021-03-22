58717aa51fee7f9eff5fab2d5542f7cefc0ec60c
Sebastian <31987608+House945@users.noreply.github.com>
2021-03-22 13:03:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Javascript Document
+
+//outputs text to the screen
+document.write("<h1>Kurs Javascript</h1>");
+document.write("<br />");
+document.write("Kurs PHP");
+/*
+line 8
+line 9
+line 10
+etc.
+*/
