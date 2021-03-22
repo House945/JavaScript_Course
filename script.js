3dbf074f9762a44728efa1040c3de57ece0f406b
Sebastian <31987608+House945@users.noreply.github.com>
2021-03-22 14:37:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 //Javascript Document
 
-//outputs text to the screen
+/*outputs text to the screen
 document.write("<h1>Kurs Javascript</h1>");
 document.write("<br />");
-document.write("Kurs PHP");
 /*
 line 8
 line 9
 line 10
 etc.
+
+var kurs = 10;
+
+document.write(kurs);
 */
+/*next lesson
+var x = "Kurs";
+var y = "JavaScript";
+document.write(x + " " + y);
+*/
+
+/*var x = 4;
+x -= 5;
+document.write(x);*/
+
+const cena = 199;
+var cena_k = cena * 0.8;
+document.write(cena_k);
