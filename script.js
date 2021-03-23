06462afa6231261613b991c52166b12119b43598
Sebastian <31987608+House945@users.noreply.github.com>
2021-03-23 13:55:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,22 @@ document.write(x + " " + y);
 x -= 5;
 document.write(x);*/
 
+/*next lesson
 const cena = 199;
 var cena_k = cena * 0.8;
 document.write(cena_k);
+*/
+
+var x = 3;
+var y = 4;
+if(x == 1)
+
+    document.write("x ma wartosc 1");
+    //document.write("<br />");
+    //document.write("x > y");
+else if (x==2)
+    document.write("x ma wartosc 2");
+else if (x==3)
+    document.write("x ma wartosc 3");
+else
+    document.write("x ma wartosc inna niz 1,2,3");
